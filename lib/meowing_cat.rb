5fc47aaf854d0b34fe4e3840479075702870c9f4
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-19 00:12:05 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,16 @@
-## code your solution here. 
+class Cat
+ 
+  def name=(maru_cat)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+end
+  end
+
+class Dog 
+  def bark
+puts "woof!"
+end 
+  end
